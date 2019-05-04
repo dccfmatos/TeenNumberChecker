4801e6a7eb14b177f267b54ee7afe6121b08a750
dccfmatos <d.catarina.c.f@gmail.com>
2019-05-04 19:08:17 +0100

    Initial commit

---
+++ src/TeenNumberChecker.java
@@ -0,0 +1,19 @@
+public class TeenNumberChecker {
+
+    public static boolean hasTeen(int N1, int N2, int N3){
+
+        if ((N1 >= 13 && N1 <= 19) || (N2 >= 13 &&  N2 <= 19) || (N3 >= 13 && N3 <= 19)){
+            return true;
+        }
+        return false;
+    }
+
+
+    public static boolean isTeen(int N4){
+
+        if(N4>= 13 && N4<=){
+            return true;
+        }
+        return false;
+    }
+}
